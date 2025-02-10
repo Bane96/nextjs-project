425c0682c3323a312cbdad9969d118655ead6cbd
Bane Dragic <bane.itmedia@gmail.com>
2025-02-10 17:28:44 +0100

    FEAT: VehicleDetail client component, display all vehicle data and display photos inside Swiper slider

---
+++ components/VehicleDetail.tsx
@@ -0,0 +1,82 @@
+"use client";
+import React from 'react';
+import {Vehicle} from '@/model/Vehicle';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Zoom, Navigation, Pagination} from 'swiper/modules';
+import {priceSplitter} from '@/utils/functions';
+import {VehicleCondition} from '@/enum/VehicleCondition';
+
+interface VehicleDetailProps {
+    vehicle: Vehicle;
+}
+
+const VehicleDetail = ({vehicle}: VehicleDetailProps) => {
+    return (
+        <>
+            <div className="flex flex-col md:flex-row gap-8 p-4">
+                <div className="md:w-2/3">
+                    <Swiper
+                        zoom={true}
+                        lazy="true"
+                        navigation={true}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        modules={[Zoom, Navigation, Pagination]}
+                        className="mySwiper rounded-xl"
+                    >
+                        {vehicle.images.map((img, index) => (
+                            <SwiperSlide key={index + img}>
+                                <div className="swiper-zoom-container">
+                                    <img loading="lazy"
+                                         src={img}
+                                         alt={vehicle.model}
+                                    />
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+                <section className="md:w-2/3 p-4 bg-gray-100 rounded-lg">
+                    <h1 className="text-2xl font-bold mb-2">{vehicle?.brand} {vehicle?.model} {vehicle.condition === VehicleCondition.NEW && `(NEW)`}</h1>
+                    <p>Price: <span
+                        className="font-semibold">€{priceSplitter(vehicle?.price)}</span></p>
+                    <p>Year: <span className="font-semibold">{vehicle?.year}</span></p>
+                    <p>Mileage: <span
+                        className="font-semibold">{priceSplitter(vehicle?.kilometer_count)} km</span></p>
+                    <p>Location: <span
+                        className="font-semibold">{vehicle?.location}</span></p>
+                    <p>Color: <span
+                        className="font-semibold">{vehicle?.color}</span></p>
+                    <p>Drivetrain: <span
+                        className="font-semibold">{vehicle?.drivetrain}</span></p>
+                    <p>Seats: <span
+                        className="font-semibold">{vehicle?.seats}</span></p>
+                </section>
+            </div>
+            <div className="flex flex-col md:flex-row gap-8 p-4">
+                <section className="md:w-1/2 p-4 bg-gray-100 rounded-lg">
+                    <h2 className="text-xl font-semibold mb-2">Additional Vehicle Details</h2>
+                    <p>Range: <span
+                        className="font-semibold"> {vehicle.range_km} km </span></p>
+                    <p>Battery Capacity: <span
+                        className="font-semibold">{vehicle.battery_capacity_kWh} kWh</span></p>
+                    <p>Charging Speed: <span
+                        className="font-semibold">{vehicle.charging_speed_kW} kW</span></p>
+                    <p>Autopilot: <span
+                        className="font-semibold">{vehicle.autopilot ? 'Yes' : 'No'}</span></p>
+                </section>
+                {vehicle.accidents &&
+                    <section className="md:w-1/2 p-4 bg-red-100 rounded-lg">
+                        <h2 className="text-xl font-semibold mb-2">Accident history</h2>
+                        <p>{vehicle.accident_description}</p>
+                    </section>
+                }
+            </div>
+        </>
+
+    )
+        ;
+};
+
+export default VehicleDetail;
