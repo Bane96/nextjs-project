4f393fb839a24d40429f37bb9c262fdfdb97ff3c
Bane Dragic <bane.itmedia@gmail.com>
2025-02-10 16:52:57 +0100

    FEAT: Layout component

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import 'swiper/css';
+import 'swiper/css/zoom';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import '@/styles/global.css';
+import Navbar from '@/components/Navbar';
+import React from 'react';
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+    title: 'Accenture | electric vehicles',
+    description: 'Easily sell your electric car online.',
+}
+
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body>
+            <Navbar/>
+                <main className="container mx-auto p-4">
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
+}
