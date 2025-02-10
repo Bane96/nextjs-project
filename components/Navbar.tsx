3972b52ab17eca68dbaef147a86a6f57a6ca6294
Bane Dragic <bane.itmedia@gmail.com>
2025-02-10 13:55:24 +0100

    FEAT: Navbar component with header and nav tag

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Navbar = () => {
+    return (
+            <header className="px-5 py-3 bg-white shadow-sm">
+                <nav className="flex justify-between items-center">
+                    <Link href="/">
+                        <Image priority={false} src="/assets/logo.svg" alt="logo" width={144} height={30} />
+                    </Link>
+                </nav>
+            </header>
+    );
+};
+
+export default Navbar;
