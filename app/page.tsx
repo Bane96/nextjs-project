2c318a5f17d06e3506be41d699757a9729bd3fb6
Bane Dragic <bane.itmedia@gmail.com>
2025-02-10 16:54:50 +0100

    FEAT: Initial page component

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VehiclesPage from './vehicles';
+
+const HomePage = () => {
+    return (
+        <div>
+            <VehiclesPage/>
+        </div>
+    );
+};
+
+export default HomePage;
